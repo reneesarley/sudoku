8fdc39cd475e4c19606ea1bb64b873c3544f7c7c
renee sarley and scott fraser <renee@>
2018-07-31 13:55:57 -0700

    all boxes tests work

--- spec/entry-spec.js
+++ spec/entry-spec.js
@@ -11,16 +11,26 @@ describe ('Game', function() {
     failArray.arrayToCheck = [1, 2, 8, 4, 5, 6, 7, 8, 9];
 
     newArray = new Game(
-      [1, 2, 3, 4, 5, 6, 7, 8, 9,
-       1, 2, 3, 4, 5, 6, 7, 8, 9,
-       1, 2, 3, 4, 5, 6, 7, 8, 9,
-       1, 2, 3, 4, 5, 6, 7, 8, 9,
-       1, 2, 3, 4, 5, 6, 7, 8, 9,
-       1, 2, 3, 4, 5, 6, 7, 8, 9,
-       1, 2, 3, 4, 5, 6, 7, 8, 9,
-       1, 2, 3, 4, 5, 6, 7, 8, 9,
-       1, 2, 3, 4, 5, 6, 7, 8, 9,
-       1, 2, 3, 4, 5, 6, 7, 8, 9]);
+      // [1, 2, 3, 4, 5, 6, 7, 8, 9,
+      //  1, 2, 3, 4, 5, 6, 7, 8, 9,
+      //  1, 2, 3, 4, 5, 6, 7, 8, 9,
+      //  1, 2, 3, 4, 5, 6, 7, 8, 9,
+      //  1, 2, 3, 4, 5, 6, 7, 8, 9,
+      //  1, 2, 3, 4, 5, 6, 7, 8, 9,
+      //  1, 2, 3, 4, 5, 6, 7, 8, 9,
+      //  1, 2, 3, 4, 5, 6, 7, 8, 9,
+      //  1, 2, 3, 4, 5, 6, 7, 8, 9,
+      //  1, 2, 3, 4, 5, 6, 7, 8, 9]);
+
+        [4,2,6,5,7,1,3,9,8,
+         8,5,7,2,9,3,1,4,6,
+         1,3,9,4,6,8,2,7,5,
+         9,7,1,3,8,5,6,2,4,
+         5,4,3,7,2,6,8,1,9,
+         6,8,2,1,4,9,7,5,3,
+         7,9,4,6,3,2,5,8,1,
+         2,6,5,8,1,4,9,3,7,
+         3,1,8,9,5,7,4,6,2]);
 
       newArray.arrayToCheck = [1, 2, 3, 4, 5, 6, 7, 8, 9]
   });
@@ -32,12 +42,22 @@ describe ('Game', function() {
     expect(rowResult).toEqual(false);
   });
 
-
-  it ('should push all values to array to check', function() {
+  it ('should push all rows to array to check', function() {
     var thisArray = newArray.getAllRows();
-    // console.log(thisArray);
     expect (thisArray).toEqual(true)
 
+  });
+
+  it ('should push all cols to array to check', function() {
+    var thisArray = newArray.getAllColumns();
+    expect (thisArray).toEqual(true)
 
   });
+
+  it ('should push all boxes to array to check', function() {
+    var thisArray = newArray.getAllBoxes();
+    expect (thisArray).toEqual(true)
+
+  });
+
 });
--- src/game.js
+++ src/game.js
@@ -18,7 +18,7 @@ Game.prototype.arrayCheck = function () {
 
 Game.prototype.getAllRows = function () {
 
-  for (var i = 0; i < 72; i+=9){
+  for (var i = 0; i <= 72; i+=9){
        this.arrayToCheck = [];
        this.arrayToCheck.push(this.mainArray[i])
        this.arrayToCheck.push(this.mainArray[i+1])
@@ -29,10 +29,55 @@ Game.prototype.getAllRows = function () {
        this.arrayToCheck.push(this.mainArray[i+6])
        this.arrayToCheck.push(this.mainArray[i+7])
        this.arrayToCheck.push(this.mainArray[i+8])
+       console.log(this.arrayToCheck);
 
        if (this.arrayCheck() == false){
          return false;
        }
+}
+return true;
+
+};
+
+Game.prototype.getAllColumns = function () {
+
+  for (var i = 0; i < 9; i++){
+       this.arrayToCheck = [];
+       this.arrayToCheck.push(this.mainArray[i])
+       this.arrayToCheck.push(this.mainArray[i+9])
+       this.arrayToCheck.push(this.mainArray[i+18])
+       this.arrayToCheck.push(this.mainArray[i+27])
+       this.arrayToCheck.push(this.mainArray[i+36])
+       this.arrayToCheck.push(this.mainArray[i+45])
+       this.arrayToCheck.push(this.mainArray[i+54])
+       this.arrayToCheck.push(this.mainArray[i+63])
+       this.arrayToCheck.push(this.mainArray[i+72])
+       if (this.arrayCheck() == false){
+         return false;
+       }
+}
+return true;
+
+};
+
+Game.prototype.getAllBoxes= function () {
+
+  for (var i = 0; i < 62; i+=3){
+    if (i == 0 || i == 3 || i == 6 || i == 27 || i == 30 || i == 33 || i == 54 || i == 57 || i == 60)
+    {    this.arrayToCheck = [];
+         this.arrayToCheck.push(this.mainArray[i])
+         this.arrayToCheck.push(this.mainArray[i+1])
+         this.arrayToCheck.push(this.mainArray[i+2])
+         this.arrayToCheck.push(this.mainArray[i+9])
+         this.arrayToCheck.push(this.mainArray[i+10])
+         this.arrayToCheck.push(this.mainArray[i+11])
+         this.arrayToCheck.push(this.mainArray[i+18])
+         this.arrayToCheck.push(this.mainArray[i+19])
+         this.arrayToCheck.push(this.mainArray[i+20])
+         if (this.arrayCheck() == false){
+           return false;
+       }
+     }
 }
 return true;
 
