fc8d0b8c44fcf4777a46868e8f8ec3bbc445e815
Renee Sarley <reneesarley@gmail.com>
2018-08-01 09:51:13 -0700

    update game object to class

--- spec/game-spec.js
+++ spec/game-spec.js
@@ -1,27 +1,24 @@
 import {Game} from '../src/game';
 
 describe ('Game', function() {
-  var winningGame;
-
+  let winningGame;
   beforeEach(function() {
-
-    winningGame = new Game(
-
-      [4,2,6,5,7,1,3,9,8,
-        8,5,7,2,9,3,1,4,6,
-        1,3,9,4,6,8,2,7,5,
-        9,7,1,3,8,5,6,2,4,
-        5,4,3,7,2,6,8,1,9,
-        6,8,2,1,4,9,7,5,3,
-        7,9,4,6,3,2,5,8,1,
-        2,6,5,8,1,4,9,3,7,
-        3,1,8,9,5,7,4,6,2]);
-
-    winningGame.arrayToCheck = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+    const winningSolution = [4,2,6,5,7,1,3,9,8,
+                            8,5,7,2,9,3,1,4,6,
+                            1,3,9,4,6,8,2,7,5,
+                            9,7,1,3,8,5,6,2,4,
+                            5,4,3,7,2,6,8,1,9,
+                            6,8,2,1,4,9,7,5,3,
+                            7,9,4,6,3,2,5,8,1,
+                            2,6,5,8,1,4,9,3,7,
+                            3,1,8,9,5,7,4,6,2]
+     winningGame = new Game(winningSolution);
+     winningGame.arrayToCheck = [1, 2, 3, 4, 5, 6, 7, 8, 9];
   });
 
   it ('should test if set of 9 numbers contains correct numbers', function() {
     var rowResult = winningGame.arrayCheck();
+    console.log(winningGame.mainArray);
     expect(rowResult).toEqual(true);
   });
 
