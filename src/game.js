ba9c0764f9cdd2ae6e052712234dfe23ab0beadc
Renee Sarley <reneesarley@gmail.com>
2018-08-01 10:35:16 -0700

    update getAllRow to accomodate an array length of any multiple of 9

--- src/game.js
+++ src/game.js
@@ -1,91 +1,94 @@
-export function Game(mainArray) {
-  this.mainArray = mainArray;
-  this.arrayToCheck = [];
-}
-
-Game.prototype.solution = function() {
-  if (this.mainArray.includes(NaN))
-  {
-    return alert('missed something!');
+export class Game {
+  constructor(usersSolution) {
+    this.usersSolution= usersSolution;
+    this.arrayToCheck = []
   }
-  else if (this.getAllRows() && this.getAllColumns() && this.getAllColumns())
-  {
-    return alert("darn, you're smart");
-  }
-};
 
-
-Game.prototype.arrayCheck = function () {
-  for (var r = 1; r < 9 ; r++){
-    if (!(this.arrayToCheck.includes(r)))
+  solution() {
+    if (this.usersSolution.includes(NaN))
     {
-      return false;
+      return alert('missed something!');
     }
-  }
-  return true;
-};
-
-
-Game.prototype.getAllRows = function () {
-  for (var i = 0; i <= 72; i+=9){
-    this.arrayToCheck = [];
-    this.arrayToCheck.push(this.mainArray[i]);
-    this.arrayToCheck.push(this.mainArray[i+1]);
-    this.arrayToCheck.push(this.mainArray[i+2]);
-    this.arrayToCheck.push(this.mainArray[i+3]);
-    this.arrayToCheck.push(this.mainArray[i+4]);
-    this.arrayToCheck.push(this.mainArray[i+5]);
-    this.arrayToCheck.push(this.mainArray[i+6]);
-    this.arrayToCheck.push(this.mainArray[i+7]);
-    this.arrayToCheck.push(this.mainArray[i+8]);
-    if (this.arrayCheck() == false){
-      return false;
+    else if (this.getAllRows() && this.getAllColumns() && this.getAllBoxes())
+    {
+      return alert("darn, you're smart");
     }
-  }
-  return true;
+  };
 
-};
 
-Game.prototype.getAllColumns = function () {
-  for (var i = 0; i < 9; i++){
-    this.arrayToCheck = [];
-    this.arrayToCheck.push(this.mainArray[i]);
-    this.arrayToCheck.push(this.mainArray[i+9]);
-    this.arrayToCheck.push(this.mainArray[i+18]);
-    this.arrayToCheck.push(this.mainArray[i+27]);
-    this.arrayToCheck.push(this.mainArray[i+36]);
-    this.arrayToCheck.push(this.mainArray[i+45]);
-    this.arrayToCheck.push(this.mainArray[i+54]);
-    this.arrayToCheck.push(this.mainArray[i+63]);
-    this.arrayToCheck.push(this.mainArray[i+72]);
-    if (this.arrayCheck() == false){
-      return false;
+  arrayCheck() {
+    for (var r = 1; r < 9 ; r++){
+      if (!(this.arrayToCheck.includes(r)))
+      {
+        return false;
+      }
     }
-  }
-  return true;
-};
+    return true;
+  };
 
-Game.prototype.getAllBoxes= function () {
-  for (var i = 0; i < 62; i+=3){
-    if (i == 0 || i == 3 || i == 6 || i == 27 || i == 30 || i == 33 || i == 54 || i == 57 || i == 60){
+
+    getAllRows() {
+    for (var i = 0; i <= this.usersSolution.length - 9; i+=9){
       this.arrayToCheck = [];
-      this.arrayToCheck.push(this.mainArray[i]);
-      this.arrayToCheck.push(this.mainArray[i+1]);
-      this.arrayToCheck.push(this.mainArray[i+2]);
-      this.arrayToCheck.push(this.mainArray[i+9]);
-      this.arrayToCheck.push(this.mainArray[i+10]);
-      this.arrayToCheck.push(this.mainArray[i+11]);
-      this.arrayToCheck.push(this.mainArray[i+18]);
-      this.arrayToCheck.push(this.mainArray[i+19]);
-      this.arrayToCheck.push(this.mainArray[i+20]);
+      this.arrayToCheck.push(this.usersSolution[i]);
+      this.arrayToCheck.push(this.usersSolution[i+1]);
+      this.arrayToCheck.push(this.usersSolution[i+2]);
+      this.arrayToCheck.push(this.usersSolution[i+3]);
+      this.arrayToCheck.push(this.usersSolution[i+4]);
+      this.arrayToCheck.push(this.usersSolution[i+5]);
+      this.arrayToCheck.push(this.usersSolution[i+6]);
+      this.arrayToCheck.push(this.usersSolution[i+7]);
+      this.arrayToCheck.push(this.usersSolution[i+8]);
       if (this.arrayCheck() == false){
         return false;
       }
     }
+    return true;
+
+  };
+
+  getAllColumns() {
+  for (var i = 0; i < 9; i++){
+    this.arrayToCheck = [];
+    this.arrayToCheck.push(this.usersSolution[i]);
+    this.arrayToCheck.push(this.usersSolution[i+9]);
+    this.arrayToCheck.push(this.usersSolution[i+18]);
+    this.arrayToCheck.push(this.usersSolution[i+27]);
+    this.arrayToCheck.push(this.usersSolution[i+36]);
+    this.arrayToCheck.push(this.usersSolution[i+45]);
+    this.arrayToCheck.push(this.usersSolution[i+54]);
+    this.arrayToCheck.push(this.usersSolution[i+63]);
+    this.arrayToCheck.push(this.usersSolution[i+72]);
+    if (this.arrayCheck() == false){
+      return false;
+    }
   }
   return true;
 };
 
+  getAllBoxes() {
+    for (var i = 0; i < 62; i+=3){
+      if (i == 0 || i == 3 || i == 6 || i == 27 || i == 30 || i == 33 || i == 54 || i == 57 || i == 60){
+        this.arrayToCheck = [];
+        this.arrayToCheck.push(this.usersSolution[i]);
+        this.arrayToCheck.push(this.usersSolution[i+1]);
+        this.arrayToCheck.push(this.usersSolution[i+2]);
+        this.arrayToCheck.push(this.usersSolution[i+9]);
+        this.arrayToCheck.push(this.usersSolution[i+10]);
+        this.arrayToCheck.push(this.usersSolution[i+11]);
+        this.arrayToCheck.push(this.usersSolution[i+18]);
+        this.arrayToCheck.push(this.usersSolution[i+19]);
+        this.arrayToCheck.push(this.usersSolution[i+20]);
+        if (this.arrayCheck() == false){
+          return false;
+        }
+      }
+    }
+    return true;
+  }
+
+
+};
 export function makeGame() {
   while (true){
     var array = []
@@ -105,7 +108,7 @@ export function makeGame() {
   //     var array = []
   //     for (var i = 0; i <= 8 ; i++ ){
   //       var rowArray = []
-  //       var newNumber =(Math.floor((Math.random() * 9) + 1));
+  //       var newNumber =(Math.floor((Math.random() * 9) + 1))
   //       if(!(rowArray.inclueds(newNumber)))
   //       {
   //         rowArray.push(newNumber);
