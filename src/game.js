b9bf758b9b1e3f46c2df5b34b374fe9f08f7c716
Student <example@example.com>
2018-08-02 11:13:07 -0700

    update checkAllColumns method

--- src/game.js
+++ src/game.js
@@ -60,14 +60,20 @@ export class Game {
   };
 
   checkAllColumns(solutionToCheck) {
+    console.log(`this is the solution that is being checked ${solutionToCheck}`);
     for (let i = 0; i < 9; i++){
       this.arrayToCheck = [];
+      console.log(`the current i value is ${i}`);
       for (let n = 0; n <= solutionToCheck.length - 9; n+=9)
       {
-        this.arrayToCheck.push(solutionToCheck[n]);
-      }
-      if (this.arrayCheck() == false){
-        return false;
+        if(!(this.arrayToCheck.indexOf(solutionToCheck[n + i])=== -1)){
+          console.log(`the current index value is ${n + i}`)
+          console.log(`the failing index is ${solutionToCheck[n]}`);
+          return false;
+        }
+          console.log(`the current index value is ${n + i}`)
+          this.arrayToCheck.push(solutionToCheck[n + i]);
+          console.log(`the current array is ${this.arrayToCheck}`);
       }
     }
     return true;
@@ -82,7 +88,11 @@ export class Game {
         for (let j = 0; j <= indexesInBox.length; j++)
         {
           let currentIndex = indexesInBox[j]
-          if(!(solutionToCheck[i+currentIndex] == null))
+          if(!(this.arrayToCheck.indexOf(solutionToCheck[i+currentIndex])===-1))
+          {
+            return false
+          }
+          else if(!(solutionToCheck[i+currentIndex] == null) )
           this.arrayToCheck.push(solutionToCheck[i+currentIndex]);
         }
         if (this.arrayCheck() == false){
@@ -96,7 +106,7 @@ export class Game {
   makeGameBoard() {
     let counter = 0;
     this.gameBoard = [];
-    while (counter < 81){
+    while (counter < 9){
       this.arrayToCheck =[];
       let options =[1, 2, 3, 4, 5, 6, 7, 8, 9]
       for (let i = 0; i <= 8 ; i++ ){
@@ -107,16 +117,46 @@ export class Game {
       for (let i = 0; i < this.arrayToCheck.length; i++ )
       {
         this.gameBoard.push(this.arrayToCheck[i]);
+        console.log(`add potential row`);
       }
-      let updatedGameBoardFailsColumnCheck = this.checkAllColumns(this.gameBoard)
-      let updatedGameBoardFailsBoxCheck = this.getAllBoxes(this.gameBoard)
-      if(!(updatedGameBoardFailsColumnCheck) || !(updatedGameBoardFailsBoxCheck))
+      if(!(this.checkAllColumns(this.gameBoard)))
       {
         this.gameBoard.splice((this.gameBoard.length-9), 9);
+      }
 
-      } else {
-        counter = this.gameBoard.length
+      // } else if(!(this.getAllBoxes(this.gameBoard))){
+      //
+      //   this.gameBoard.splice((this.gameBoard.length-9), 9);
+      // }
+      else {
+        counter += 1
       }
     }
+      console.log(`this is the final gamenoard: ${this.gameBoard}`);
   }
 };
+
+// this was the result at 9:26- this ran really quickly, first attempt after building
+// 2,1,3,9,8,4,7,6,5,
+// 4,6,8,7,5,3,1,9,2,
+// 9,5,7,2,6,1,3,8,4,
+// 6,7,5,4,1,8,2,3,9,
+// 1,3,2,5,9,6,7,8,4,
+// 8,4,9,3,2,7,1,6,5,
+// 5,2,1,7,9,4,6,3,8,
+// 7,8,9,3,6,2,1,4,5,
+// 3,6,4,5,1,8,2,7,9
+
+//attempt at 9:28 - ran and showed console log
+
+// atempt at 9:30 - didn't even show console log
+
+// attempt 9:32
+
+
+
+
+// 8,2,5,3,7,4,1,9,6,
+// 6,4,9,1,8,2,5,7,3,
+// 1,3,7,6,9,5,4,2,8,
+// 5,4,9,2,6,8,3,7,1,2,7,8,3,4,1,6,5,9,3,1,6,5,7,9,8,2,4,4,6,1,5,9,3,8,7,2,9,3,8,2,1,7,4,5,6,7,2,5,8,6,4,1,3,9
